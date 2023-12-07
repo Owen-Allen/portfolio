938979b1b9920bfb663979b07ae0deab0d7fe8a0
Owen-Allen <73844263+Owen-Allen@users.noreply.github.com>
2023-12-07 18:05:55 -0500

    switched to sendgrid

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,21 +1,14 @@
-
-
+import sgMail from '@sendgrid/mail';
 
 export async function POST(req: Request) {
     console.log("RECIEVED POST")
-    let nodemailer = require('nodemailer')
+    let SENDGRID_API_KEY = process.env.SENDGRID_API_KEY != undefined ? process.env.SENDGRID_API_KEY : "";
+    console.log(SENDGRID_API_KEY)
+    sgMail.setApiKey(SENDGRID_API_KEY);
     let body = await req.json()
-    const transporter = nodemailer.createTransport({
-      port: 465,
-      host: "smtp.gmail.com",
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.PASSWORD,
-      },
-      secure: true,
-    })
-    const mailData = {
-      from: 'demo email',
+
+    const msg = {
+      from: 'autoemailer0@gmail.com',
       to: 'owenallen.2000@gmail.com',
       subject: `Message From ${body.name}`,
       text: body.message + " | Sent from: " + body.email,
@@ -23,16 +16,32 @@ export async function POST(req: Request) {
       ${body.email}</p>`
     }
 
-    console.log(mailData)
-    transporter.sendMail(mailData, function (err: any, info: any) {
-      if(err){
-        console.log('error')
-        console.log(err)
-        return new Response(null, { status: 500, statusText: "NOT WORKING" })
-      }else{
-        console.log('hello?')
-        console.log(info)
-    }})
-
-    return new Response(null, { status: 200, statusText: "working!" })
+    try {
+      await sgMail.send(msg);
+      return new Response(null, { status: 200, statusText: 'Email sent successfully' });
+  } catch (error) {
+      console.error('Error sending email:', error);
+      console.log(error)
+      return new Response(null, { status: 500, statusText: 'Internal Server Error' });
+  }
 }
+
+// const transporter = nodemailer.createTransport({
+//   port: 465,
+//   host: "smtp.gmail.com",
+//   auth: {
+//     user: process.env.EMAIL,
+//     pass: process.env.PASSWORD,
+//   },
+//   secure: true,
+// })
+
+// transporter.sendMail(mailData, function (err: any, info: any) {
+//   if(err){
+//     console.log('error')
+//     console.log(err)
+//     return new Response(null, { status: 500, statusText: "NOT WORKING" })
+//   }else{
+//     console.log('hello?')
+//     console.log(info)
+// }})
