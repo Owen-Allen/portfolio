3912723bb7462dc21047b8c0c00fe9d2a545ab2f
Owen-Allen <73844263+Owen-Allen@users.noreply.github.com>
2023-12-07 23:40:57 -0500

    button now pushes to /projects instead of back

--- components/backButton.tsx
+++ components/backButton.tsx
@@ -5,7 +5,7 @@ export default function BackButton() {
     const router = useRouter()
 
     return (
-        <button type="button" onClick={() => router.back()} className="rounded-full w-12 flex items-center justify-center sm:px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border gap-x-2 sm:w-auto hover:bg-gray-100">
+        <button type="button" onClick={() => router.push('/projects')} className="rounded-full w-12 flex items-center justify-center sm:px-5 py-2 text-sm text-gray-700 transition-colors duration-200 bg-white border gap-x-2 sm:w-auto hover:bg-gray-100">
             <svg className="sm:w-5 h-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
             </svg>
